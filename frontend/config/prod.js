a5530bb78b2bb1d8c854d3275e5dc8ba6d2c94d0
QIJINWEI <66395357+QIJINWEI@users.noreply.github.com>
2025-11-04 12:22:57 +0800

    feat: add frontend Taro configuration files

---
+++ frontend/config/prod.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    NODE_ENV: '"production"'
+  },
+  defineConstants: {
+    API_BASE_URL: '"https://api.yourdomain.com"'
+  },
+  mini: {},
+  h5: {
+    publicPath: '/homework-helper/'
+  }
+}
